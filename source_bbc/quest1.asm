2ce279b46fe2ec13ebc0aadc12533aee12e35700
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-09 05:00:00 +0100

    correct moth sprite identification

--- source_bbc/quest1.asm
+++ source_bbc/quest1.asm
@@ -299,16 +299,16 @@ ORG &9000
 .small_bouncing_robot_direction_frame_3
     EQUB &11, &22, &07, &6F, &69, &0F, &01, &07, &CC, &06, &6F, &6F, &6F, &06, &00, &00
     EQUB &00, &06, &6F, &6F, &6F, &07, &00, &00, &00, &00, &00, &00, &0E, &6F, &6F, &06
-.moth_wings_raised_frame
+.unidentified_flying_creature_frame_0
     EQUB &0C, &86, &C3, &61, &30, &10, &00, &00, &88, &44, &22, &0F, &6F, &87, &E3, &22
     EQUB &11, &22, &44, &0F, &6F, &1E, &4C, &44, &03, &16, &3C, &68, &C0, &80, &00, &00
-.moth_wings_lowered_frame
+.unidentified_flying_creature_frame_1
     EQUB &00, &00, &00, &03, &34, &70, &00, &00, &22, &22, &22, &0F, &6F, &87, &23, &22
     EQUB &44, &44, &44, &0F, &6F, &1E, &4C, &44, &00, &00, &00, &0E, &E1, &F0, &00, &00
-.humanoid_robot_and_hazard_frame_0
+.moth_and_hazard_frame_0
     EQUB &02, &05, &00, &00, &33, &CC, &11, &EE, &00, &00, &68, &61, &CF, &03, &CD, &01
     EQUB &00, &00, &61, &68, &3F, &0C, &3B, &08, &04, &0A, &00, &00, &CC, &33, &88, &77
-.humanoid_robot_and_hazard_frame_1
+.moth_and_hazard_frame_1
     EQUB &03, &00, &00, &EE, &11, &CC, &33, &00, &08, &04, &60, &61, &CF, &03, &CD, &01
     EQUB &01, &02, &60, &68, &3F, &0C, &3B, &08, &0C, &00, &00, &77, &88, &33, &CC, &00
 .player_upper_facing_right_frame
@@ -2678,9 +2678,9 @@ ORG initialise_room_entity_from_table
 ;
 ; $1221 selects the four-byte graphic descriptor copied from $1FDF. The three
 ; selected graphic pairs and their rooms are
-;   moth                      B5 D0 A1 G2 E3 D6
+;   unidentified flying form B5 D0 A1 G2 E3 D6
 ;   striped lift/platform     C3 F1 C1 B3 B6 E2 A2 C7
-;   humanoid robot/hazard     C5 E5 F4 E4 H7 D7
+;   moth/hazard               C5 E5 F4 E4 H7 D7
 ; See analysis/room_map.md for the rooms alongside what the account says of
 ; them. These are decoded graphic identities; the shared state machine means a
 ; visual identity alone must not be used to infer movement or collision rules.
@@ -11633,9 +11633,9 @@ ORG enemy_graphic_descriptor_table
 ; Four decoded graphic pairs selected by room-entity type at $1FB9. The fourth
 ; jellyfish pair is present but no six-byte room record selects it.
 .enemy_graphic_descriptor_table_source
-    EQUW &0480, &04A0 ; moth_wings_raised/lowered_frame
+    EQUW &0480, &04A0 ; unidentified_flying_creature_frame_0/1
     EQUW &0640, &0660 ; striped_lift_platform_frame_0/1
-    EQUW &04C0, &04E0 ; humanoid_robot_and_hazard_frame_0/1
+    EQUW &04C0, &04E0 ; moth_and_hazard_frame_0/1
     EQUW &0620, &0600 ; jellyfish_frame_0/1 (unselected here)
 .enemy_graphic_descriptor_table_source_end
 ASSERT enemy_graphic_descriptor_table_source = enemy_graphic_descriptor_table
